7039de511bd649e66fd7ee63ad1de8d7f5b499b8
Noemi <noemi.caggiano@gmail.com>
2022-09-06 16:04:16 +0100

    feat: added state and components to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import React, {useState} from 'react';
 import './App.css';
+import { Form } from './components/Form';
 
 export default function App() {
+
+    const [registerUser, setRegisterUser] = useState({ username: '', password: ''})
+    const [loginUser, setLoginUser] = useState({ username: '', password: ''})
+
     return (
         <div className="App">
-
+            <Form user={registerUser} set={setRegisterUser}/>
+            <Form user={loginUser} set={setLoginUser}/>
         </div>
     );
 }
