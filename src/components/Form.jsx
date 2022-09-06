c754c170643ae1a1f5604806233b51d4e01de920
Noemi <noemi.caggiano@gmail.com>
2022-09-06 16:04:28 +0100

    feat: components set up

---
+++ src/components/Form.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { Input } from "./Input"
+
+export const Form = ({ user, set }) => {
+  
+  const handleSubmit = () => {}
+
+  return (
+    <Form>
+      handleSubmit={handleSubmit}
+      <Input />
+      <Input />
+    </Form>
+  )
+}
